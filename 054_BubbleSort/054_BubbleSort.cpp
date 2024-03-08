36bf7fae7a5bd56dfcda9fde2b83984a76e8466b
nisrinasals <nisrinahikmawan@gmail.com>
2024-03-08 15:31:09 +0700

    menambah comment

--- 054_BubbleSort/054_BubbleSort.cpp
+++ 054_BubbleSort/054_BubbleSort.cpp
@@ -2,13 +2,13 @@
 #include <string>
 using namespace std;
 
-int arr[20];
-int n;
+int arr[20];	//deklarasi variabel global array a dengan ukuran 20
+int n;	//deklarasi variable global n untuk menyimpan banyaknya elemen array 
 
 void input() { //prosedur untuk input
 	while (true) {
 		cout << "Masukkan banyaknya elemen pada array : ";	//output ke layar
-		cin >> n;
+		cin >> n;	//input dari pengguna
 		if (n <= 20)	//jika n kurang dari atau sama dengan 20
 			break;	//keluar dari loop
 		else {	//jika n lebih dari 20
@@ -22,11 +22,13 @@ void input() { //prosedur untuk input
 
 	for (int i = 0; i < n; i++) {	//looping dengan i dimulai dari 0 hingga n-1
 		cout << "Data ke-" << (i + 1) << ": ";	//output ke layar
-		cin >> arr[i];
+		cin >> arr[i];	//output dari pengguna
 	}
 }
 
+void bubblesortArray() {	
 
+}
 
 
 int main() {
